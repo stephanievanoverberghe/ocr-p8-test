c1d5a92598f3f8e00568cdb6f2b423dc86eb631f
Stéphanie Vanoverberghe <orangestreet59123@gmail.com>
2024-08-27 16:35:22 +0200

    fix: add exercice 1

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,16 @@
 //votre code ici
 
+const pairNumbers = (min, max) => {
+    let numbers = [];
+
+    for (let i = min; i <= max; i++) {
+        if (i % 2 === 0) {
+            numbers.push(i);
+        }
+    }
+    return numbers.join(',');
+}
+
+pairNumbers(1, 10);
+
 export default pairNumbers
